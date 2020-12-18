7e1897ad22b631fe40888191c96477ffc0c7c1d0
Olivia Park <oliviaypark113@gmail.com>
2020-12-18 01:22:43 -0500

    change bk to http

--- script.js
+++ script.js
@@ -8,7 +8,6 @@
  var zipCodeweather;
  var query_param;
  var currentLocation;
- var defaultCity = "new york"
  var lat;
  var lon;
  var uvIndexspan = $("#uv-index")
@@ -20,10 +19,10 @@
  function lastSavedcity() {
 
     if(cityVariable){
-         weather ="https://api.openweathermap.org/data/2.5/weather?q=" +cityVariable +"&units=imperial"+"&appid=" + appID;
+         weather ="http://api.openweathermap.org/data/2.5/weather?q=" +cityVariable +"&units=imperial"+"&appid=" + appID;
        
         
-        forecast ="https://api.openweathermap.org/data/2.5/forecast?q=" +cityVariable+"&units=imperial"+"&APPID=" + appID;
+        forecast ="http://api.openweathermap.org/data/2.5/forecast?q=" +cityVariable+"&units=imperial"+"&APPID=" + appID;
 
         getWeatherData()
         getForecastData()
@@ -42,18 +41,18 @@ $(".query_btn").on("click",function(){
 
     if($(this).prev().attr("placeholder") == "City"){
          
-         weather ="https://api.openweathermap.org/data/2.5/weather?q=" + query_param +"&units=imperial"+"&appid=" + appID;
+         weather ="http://api.openweathermap.org/data/2.5/weather?q=" + query_param +"&units=imperial"+"&appid=" + appID;
 
-         forecast ="https://api.openweathermap.org/data/2.5/forecast?q=" + query_param +"&units=imperial"+"&APPID=" + appID;
+         forecast ="http://api.openweathermap.org/data/2.5/forecast?q=" + query_param +"&units=imperial"+"&APPID=" + appID;
 
         getWeatherData()
         getForecastData()
     }
 
     else if($(this).prev().attr("placeholder")=="Zip Code"){
-         zipCodeweather ="https://api.openweathermap.org/data/2.5/weather?zip=" + query_param +"&units=imperial"+"&appid=" + appID;
+         zipCodeweather ="http://api.openweathermap.org/data/2.5/weather?zip=" + query_param +"&units=imperial"+"&appid=" + appID;
 
-         forecast ="https://api.openweathermap.org/data/2.5/forecast?q=" + query_param +"&units=imperial"+"&APPID=" + appID;
+         forecast ="http://api.openweathermap.org/data/2.5/forecast?q=" + query_param +"&units=imperial"+"&APPID=" + appID;
         
         getZipcodeWeatherData()
         getForecastData()
@@ -68,9 +67,9 @@ $(".othercities").on("click", function(){
   
     var cityInfo = $(this).attr("data-city")
 
-    weather ="https://api.openweathermap.org/data/2.5/weather?q=" + cityInfo +"&units=imperial"+"&appid=" + appID;
+    weather ="http://api.openweathermap.org/data/2.5/weather?q=" + cityInfo +"&units=imperial"+"&appid=" + appID;
 
-    forecast ="https://api.openweathermap.org/data/2.5/forecast?q=" + cityInfo +"&units=imperial"+"&APPID=" + appID;
+    forecast ="http://api.openweathermap.org/data/2.5/forecast?q=" + cityInfo +"&units=imperial"+"&APPID=" + appID;
     
     getWeatherData()
     getForecastData()
@@ -83,7 +82,7 @@ $(".othercities").on("click", function(){
 function  getWeatherData() {
     $.getJSON(weather, function(json){
 
-         weather ="https://api.openweathermap.org/data/2.5/weather?q=" + query_param +"&units=imperial"+"&appid=" + appID;
+         weather ="http://api.openweathermap.org/data/2.5/weather?q=" + query_param +"&units=imperial"+"&appid=" + appID;
 
 
          lat = json.coord.lat;
@@ -95,7 +94,7 @@ function  getWeatherData() {
         $("#country").text(" ," + json.sys.country);
         $("#main_weather").text(json.weather[0].main);
         $("#description_weather").text(json.weather[0].description);
-        $("#weather_image").attr("src", "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+        $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
         $("#temperature").text(json.main.temp);
         $("#wind-speed").text(json.wind.speed);
         $("#humidity").text(json.main.humidity);
@@ -106,7 +105,7 @@ function  getWeatherData() {
 
 function getUVData(){
      
-    var uvEndpoint = "https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid="+appID
+    var uvEndpoint = "http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid="+appID
 
     $.getJSON(uvEndpoint, function(uvdata){
      
@@ -158,7 +157,7 @@ function renderDate(){
 function  getZipcodeWeatherData() {
     $.getJSON(zipCodeweather, function(json){
          
-        zipCodeweather ="https://api.openweathermap.org/data/2.5/weather?zip=" + query_param +"&units=imperial"+"&appid=" + appID;
+        zipCodeweather ="http://api.openweathermap.org/data/2.5/weather?zip=" + query_param +"&units=imperial"+"&appid=" + appID;
         
         lat = json.coord.lat;
         lon = json.coord.lon;
@@ -168,7 +167,7 @@ function  getZipcodeWeatherData() {
         $("#country").text(" ," + json.sys.country);
         $("#main_weather").text(json.weather[0].main);
         $("#description_weather").text(json.weather[0].description);
-        $("#weather_image").attr("src", "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+        $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
         $("#temperature").text(json.main.temp);
         $("#wind-speed").text(json.wind.speed);
         $("#humidity").text(json.main.humidity);
@@ -178,34 +177,34 @@ function  getZipcodeWeatherData() {
 function getForecastData(){
     $.getJSON(forecast, function(json){
 
-         forecast ="https://api.openweathermap.org/data/2.5/forecast?q=" + query_param +"&units=imperial"+"&APPID=" + appID;
+         forecast ="http://api.openweathermap.org/data/2.5/forecast?q=" + query_param +"&units=imperial"+"&APPID=" + appID;
           
       for (var i=7; i<json.list.length; i+=8 ){
        
         
            $("#day1-date").text("Date: "+json.list[7].dt_txt);
-           $("#day1-icon").attr("src",  "https://openweathermap.org/img/w/" + json.list[7].weather[0].icon + ".png");
+           $("#day1-icon").attr("src",  "http://openweathermap.org/img/w/" + json.list[7].weather[0].icon + ".png");
            $("#day1-temp").text("Temp: "+json.list[7].main.temp+ " °F")
            $("#day1-humidity").text("Humidity: "+json.list[7].main.humidity+ " %")
 
            $("#day2-date").text("Date: "+json.list[15].dt_txt);
-           $("#day2-icon").attr("src",  "https://openweathermap.org/img/w/" + json.list[15].weather[0].icon + ".png");
+           $("#day2-icon").attr("src",  "http://openweathermap.org/img/w/" + json.list[15].weather[0].icon + ".png");
            $("#day2-temp").text("Temp: "+json.list[15].main.temp+ " °F")
            $("#day2-humidity").text("Humidity: "+json.list[15].main.humidity+ " %")
 
 
            $("#day3-date").text("Date: "+json.list[23].dt_txt);
-           $("#day3-icon").attr("src",  "https://openweathermap.org/img/w/" + json.list[23].weather[0].icon + ".png");
+           $("#day3-icon").attr("src",  "http://openweathermap.org/img/w/" + json.list[23].weather[0].icon + ".png");
            $("#day3-temp").text("Temp: "+json.list[23].main.temp+ " °F")
            $("#day3-humidity").text("Humidity: "+json.list[23].main.humidity + " %")
 
            $("#day4-date").text("Date: "+json.list[31].dt_txt);
-           $("#day4-icon").attr("src",  "https://openweathermap.org/img/w/" + json.list[31].weather[0].icon + ".png");
+           $("#day4-icon").attr("src",  "http://openweathermap.org/img/w/" + json.list[31].weather[0].icon + ".png");
            $("#day4-temp").text("Temp: "+ json.list[31].main.temp+ " °F")
            $("#day4-humidity").text("Humidity: "+json.list[31].main.humidity + " %")
 
            $("#day5-date").text("Date: " +json.list[39].dt_txt);
-           $("#day5-icon").attr("src",  "https://openweathermap.org/img/w/" + json.list[39].weather[0].icon + ".png");
+           $("#day5-icon").attr("src",  "http://openweathermap.org/img/w/" + json.list[39].weather[0].icon + ".png");
            $("#day5-temp").text("Temp: "+ json.list[39].main.temp + " °F")
            $("#day5-humidity").text("Humidity: " +json.list[39].main.humidity +" %")
 
